7cf04928d63139f2a575cea153d961dd4dea461d
saarahha <saarah@sas.upenn.edu>
2024-10-25 14:32:54 -0500

    trial gaussian t0prior

--- analysis/fit_villar_numpyro.py
+++ analysis/fit_villar_numpyro.py
@@ -138,9 +138,11 @@ def lc_model(t_val, Y_unc_val, Y_observed_val=None):
     uniformbeta_low = -jnp.max(Y_observed_val) / 150
     Beta = numpyro.sample("Beta", dist.Normal(0, uniformbeta_low * (-0.3)))
 
-    t0 = numpyro.sample("t0", dist.Uniform(
-        low=jnp.array(t_val)[jnp.argmax(Y_observed_val)] - 25,
-        high=jnp.array(t_val)[jnp.argmax(Y_observed_val)] + 25))
+    #t0 = numpyro.sample("t0", dist.Uniform(
+    #    low=jnp.array(t_val)[jnp.argmax(Y_observed_val)] - 25,
+    #    high=jnp.array(t_val)[jnp.argmax(Y_observed_val)] + 25))
+    t0_uniform_prior_center = jnp.array(t_val)[jnp.argmax(Y_observed_val)]
+    t0 = numpyro.sample("t0", dist.Gaussian(mu=t0_uniform_prior_center - 10, sigma=10))
 
     sigma_est = jnp.sqrt(jnp.mean(Y_unc_val ** 2))
     scalar = numpyro.sample("scalar", dist.TruncatedNormal(loc=0, scale=sigma_est,
